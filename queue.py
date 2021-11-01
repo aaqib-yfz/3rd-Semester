6a1caf1a14683196ad00a959a20887b8c65797fa
aaqib-yfz <aaqibshaheer2001@gmail.com>
2021-11-01 11:51:54 -0700

    random

---
+++ queue.py
@@ -0,0 +1,15 @@
+class queue:
+    def __init__(self):
+        self.items=[]
+    def isEmpty(self):
+        return self.items==[]
+    def enqueue(self,items):
+        self.items.insert(0,items)
+    def dequeue(self):
+        return self.items.pop()
+    def size(self):
+        return len(self.items)
+q = queue()
+q.enqueue(4)
+q.enqueue('dog')
+print(q.size())
--- stack_implement.py
+++ stack_implement.py
@@ -1,6 +1,6 @@
 class Stack: 
     def __init__(self): 
-        self.elements = [] 
+        self.elem = [] 
     
     def push(self, data): 
         self.elements.append(data) 
